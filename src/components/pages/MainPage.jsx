79b37646c5983ee29249d0ea4414c0782071bef8
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-21 22:05:21 -0600

    main page hero completed

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -5,12 +5,16 @@ import Download from '../../assets/icons/download.svg'
 function MainPage() {
   return (
     <article className="hero-container">
-      <h1 className="hero-title">Hello! I'm <br/>
-        <NavLink to="about" className="primary hero-title hero-name"> Victor Gonzalez</NavLink>
-        <br/>Full-stack Dev</h1>
-      <p>This is my personal portfolio</p>
-      <NavLink to="projects" className="btn btn--blue">Projects</NavLink>
-      <button className="btn btn--outline"><img className="icon" src={Download} alt="download" /> Resume</button>
+      <div className="hero-content">
+        <h1 className="hero-title">Hello! I'm <br/>
+          <NavLink to="about" className="primary hero-title hero-name"> Victor Gonzalez</NavLink>
+          <br/>Full-stack Dev</h1>
+        <p>This is my personal portfolio</p>
+        <div className="hero-button-container">
+        <NavLink to="projects" className="btn btn--blue">Projects</NavLink>
+        <button className="btn btn--outline"><img className="icon" src={Download} alt="download" /> Resume</button>
+        </div>
+      </div>
 
     </article>
   )
