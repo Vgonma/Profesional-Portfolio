cb4086e02a08e833041897cf772001e7ba75ae26
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-21 21:45:14 -0600

    mobile main page hero finished

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Download from '../../assets/icons/download.svg'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <article className="hero-container">
+      <h1 className="hero-title">Hello! I'm <br/>
+        <NavLink to="about" className="primary hero-title hero-name"> Victor Gonzalez</NavLink>
+        <br/>Full-stack Dev</h1>
+      <p>This is my personal portfolio</p>
+      <NavLink to="projects" className="btn btn--blue">Projects</NavLink>
+      <button className="btn btn--outline"><img className="icon" src={Download} alt="download" /> Resume</button>
+
+    </article>
   )
 }
 
