4937d370d56addaf07d5e5b4d4de2336445241b7
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-22 20:56:44 -0600

    About me card complete

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import Download from '../../assets/icons/download.svg'
+import AboutMe from '../AboutMe'
 
 function MainPage() {
   return (
-    <article className="hero-container">
-      <div className="hero-content">
-        <h1 className="hero-title">Hello! I'm <br/>
-          <NavLink to="about" className="primary hero-title hero-name"> Victor Gonzalez</NavLink>
-          <br/>Full-stack Dev</h1>
-        <p>This is my personal portfolio</p>
-        <div className="hero-button-container">
-        <NavLink to="projects" className="btn btn--blue">Projects</NavLink>
-        <button className="btn btn--outline"><img className="icon" src={Download} alt="download" /> Resume</button>
+    <>
+      <article className="hero-container">
+        <div className="hero-content">
+          <h1 className="hero-title">Hello! I'm <br/>
+            <NavLink to="about" className="primary hero-title hero-name"> Victor Gonzalez</NavLink>
+            <br/>Full-stack Dev</h1>
+          <p>This is my personal portfolio</p>
+          <div className="hero-button-container">
+          <NavLink to="projects" className="btn btn--blue">Projects</NavLink>
+          <button className="btn btn--outline"><img className="icon" src={Download} alt="download" /> Resume</button>
+          </div>
         </div>
-      </div>
-
-    </article>
+      </article>
+      <article className="about-container">
+        <AboutMe />
+      </article>
+    </>
   )
 }
 
