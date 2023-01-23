f77d0cc9ae72e55d1962adffe4742f2e7b31bbc4
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-23 13:09:03 -0600

    Add express config

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App'
 import './index.css'
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
