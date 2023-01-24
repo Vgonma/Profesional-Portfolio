42883c324648c0963252d615732a5be8aa5d47dd
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-23 19:56:09 -0600

    update footer spacing and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import LinkedIn from '../assets/logos/Linkedin.svg'
-import GitHub from '../assets/logos/github_icon.svg'
+import GitHub from '../assets/logos/github.svg'
+import Mail from '../assets/logos/email.svg'
 
 
 function Footer() {
@@ -13,22 +14,22 @@ function Footer() {
   return (
     <div className="footer-container">
       <div className="footer">
+        <div className="footer-subsection">
         <Link to="/" className="footer-title" onClick={scrollTop}>Victor G.</Link>
+        <a href="mailto:victorgmassimi@hotmail.com">victorgmassimi@hotmail.com</a>
+        </div>
         <div className="footer-subsection">
           <Link to="/" className="footer-header" onClick={scrollTop}>Home</Link>
           <a href="#About">About me</a>
           <a href="#Projects">Projects</a>
         </div>
+
         <div className="footer-subsection">
           <Link to="contact" className="footer-header" onClick={scrollTop}>Contact</Link>
-          <a href="#">Mail</a>
-          <a href="https://www.linkedin.com/in/victor-gonzalez-massimi/" target="_blank">Linkedin</a>
-        </div>
-        <div className="footer-subsection">
-          <p className="footer-header">Social</p>
           <div className="social-icons-container">
             <a href="https://www.linkedin.com/in/victor-gonzalez-massimi/" target="_blank"><img src={LinkedIn} alt="LinkedIn" /></a>
             <a href="https://github.com/Vgonma" target="_blank"><img src={GitHub} alt="GitHub" /></a>
+            <a href="mailto:victorgmassimi@hotmail.com" target="_blank"><img src={Mail} alt="GitHub" /></a>
           </div>
         </div>
       </div>
