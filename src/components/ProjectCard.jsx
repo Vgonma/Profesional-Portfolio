5b464ee36a3277d34244021b554fe20576876a93
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-23 18:31:05 -0600

    Add project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
-function ProjectCard() {
+import projectImg from '../assets/images/projectImage.svg';
+
+function ProjectCard({ title = 'Project', image, tech = [] }) {
+  const [projectTitle, setProjectTitle] = useState(title);
+  const [projectImg, setprojectImg] = useState(image);
+  const [projectTech, setProjectTech] = useState(tech)
+
   return (
-    <div>ProjectCard</div>
+    <Link to={`projects/${projectTitle.split(' ').join('-').toLowerCase()}`} className="single-project-container">
+        <img className="project-img" src={projectImg} alt={projectTitle} />
+        <div className="project-info">
+          <h3 className="project-title">{projectTitle}</h3>
+          <div className="tech-stack-container">
+          { projectTech.map((item, i) => (
+            <div  className="tech" key={i}>{item}</div>
+            )) }
+          </div>
+        </div>
+    </Link>
   )
 }
 
-export default ProjectCard
+export default ProjectCard;
