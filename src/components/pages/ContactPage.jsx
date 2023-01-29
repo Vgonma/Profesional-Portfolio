0175322b2b7a879526b76fd70e8bc67ec479540c
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-28 18:30:12 -0600

    Contact page markup complete

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 function ContactPage() {
   return (
-    <div>Contact Page</div>
+    <div className="contact-page-container">
+      <div className="title-container">
+        <h1 className="title title--big">Contact Me</h1>
+      </div>
+      <div className="contact-form-container">
+        <form className="contact-form" action="mail.php">
+          <div className="input-container">
+            <label className="label" htmlFor="name">Name</label>
+            <input className="input" type="text" name="name" placeholder="Name" required/>
+          </div>
+          <div className="input-container">
+            <label className="label" htmlFor="email">Email</label>
+            <input className="input" type="text" name="email" placeholder="Email" required/>
+          </div>
+          <div className="input-container">
+            <label className="label" htmlFor="message">Message</label>
+            <textarea className="text-area input" name="message" placeholder="Your message here..." required></textarea>
+          </div>
+          <button className="contact-form-button btn btn--blue">Send</button>
+        </form>
+
+      </div>
+    </div>
   )
 }
 
